1a0636ee30ab8b6e4520d769bade11cafaf286e8
Arunthiga <arunthiga@gmail.com>
2026-03-08 18:39:23 +0530

    fix: full cleanup of diagnostic files

--- api/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "PHP Version: " . phpversion() . "\n";
-echo "MongoDB Extension: " . (extension_loaded('mongodb') ? "FOUND" : "NOT FOUND") . "\n";
-if (class_exists('MongoDB\Driver\Manager')) {
-    echo "MongoDB Driver Class: FOUND\n";
-} else {
-    echo "MongoDB Driver Class: NOT FOUND\n";
-}
-?>
